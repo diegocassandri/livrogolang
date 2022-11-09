8615c8f7f3f5a0614770e562148f473337e57337
Diego Cassandri <diego.cassandri@gmail.com>
2022-11-09 16:41:30 -0300

    Pointers

---
+++ Ponteiros/pointers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Carro struct {
+	Marca string //Apontando para um lugar na memória
+}
+
+func (c *Carro) MudaMarca(marca string) {
+	c.Marca = marca //Apontando para outro lugar na memória
+	fmt.Println(c.Marca)
+}
+
+func main() {
+	carro := Carro{Marca: "Fiat"}
+	carro.MudaMarca("Ford")
+	fmt.Println(carro.Marca)
+
+}
