10122f40a2bdd248c07e0fb79bc5eee5ac75c253
Diego Cassandri <diego.cassandri@gmail.com>
2022-11-28 20:40:39 -0300

    CH4

---
+++ ch4/issuesreport/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"livrogolang/ch4/github"
+	"log"
+	"os"
+	"text/template"
+	"time"
+)
+
+// Definição de Template
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User: {{.User.Login}}
+Title: {{.Title | printf "%.64s"}}
+Age: {{.CreatedAt | daysAgo}} days
+{{end}} 
+`
+
+// Função dayAgo que é passada no template acima
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+// Execução do Template
+var report = template.Must(template.New("issuelist").
+	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+	Parse(templ))
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func noMust() {
+	//!+parse
+	report, err := template.New("report").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//!-parse
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
