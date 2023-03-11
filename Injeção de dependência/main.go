1352fc00566222b2633c7cccf057ede308312095
Diego Cassandri <diego.cassandri@mercadolivre.com>
2023-03-11 14:34:58 -0300

    Dependency Injection exemple

---
+++ Injeção de dependência/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "log"
+
+type Database interface {
+	GetUser() string
+}
+
+type DefaultDatabse struct {
+	name string
+}
+
+func (db DefaultDatabse) GetUser() string {
+	db.name = "Default Database"
+	return db.name
+}
+
+type FamousDatabase struct {
+	name string
+}
+
+func (fb FamousDatabase) GetUser() string {
+	fb.name = "Famous Database"
+	return fb.name
+}
+
+type Greeter interface {
+	Greet(userName string)
+}
+
+type NiceGreeter struct{}
+
+func (ng NiceGreeter) Greet(userName string) {
+	log.Printf("Hello %s!! Nice to meet you", userName)
+}
+
+type Program struct {
+	Db      Database
+	Greeter Greeter
+}
+
+func (p Program) Execute() {
+	user := p.Db.GetUser()
+	p.Greeter.Greet(user)
+}
+
+func newProgram(db Database, greeter NiceGreeter) *Program {
+	return &Program{Db: db, Greeter: greeter}
+}
+
+func main() {
+	db := DefaultDatabse{}
+	greeter := NiceGreeter{}
+
+	p := newProgram(&db, greeter)
+
+	p.Execute()
+
+}
